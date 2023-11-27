161937324dab9b7eef3831c5b41758a5e2c89f13
Daniel Zupan <GiovanniBattista@users.noreply.github.com>
2023-11-27 19:55:44 +0100

    Updated style of SleepCompanyPage

--- src/pages/SleepCompanyPage.vue
+++ src/pages/SleepCompanyPage.vue
@@ -2,31 +2,36 @@
   <f7-page name="sleepcompany">
     <f7-navbar title="Sleep Company"></f7-navbar>
 
-    <f7-block>
-      <f7-row class="margin-bottom">
-        <f7-col>
-          <div>
-            <needs-tracker-tracking-button
-              :active="who === 'mommy'"
-              @click="onChangeSleepCompanyClicked('mommy')"
-              icon="mommy"
-              title="Mommy"
-            ></needs-tracker-tracking-button>
-          </div>
-        </f7-col>
+    <div class="display-flex flex-direction-column align-items-center justify-content-center" style="height: 100%">
+      <f7-block-title class="display-flex justify-content-center margin-bottom">Who is next?</f7-block-title>
+      <f7-block class="margin-top">
+        <f7-row>
+          <f7-col>
+            <div>
+              <needs-tracker-tracking-button
+                :active="who === 'mommy'"
+                @click="onChangeSleepCompanyClicked('mommy')"
+                icon="mommy"
+                title="Mommy"
+              ></needs-tracker-tracking-button>
+              <div class="who">Mommy</div>
+            </div>
+          </f7-col>
 
-        <f7-col>
-          <div>
-            <needs-tracker-tracking-button
-              :active="who === 'daddy'"
-              @click="onChangeSleepCompanyClicked('daddy')"
-              icon="daddy"
-              title="Daddy"
-            ></needs-tracker-tracking-button>
-          </div>
-        </f7-col>
-      </f7-row>
-    </f7-block>
+          <f7-col>
+            <div>
+              <needs-tracker-tracking-button
+                :active="who === 'daddy'"
+                @click="onChangeSleepCompanyClicked('daddy')"
+                icon="daddy"
+                title="Daddy"
+              ></needs-tracker-tracking-button>
+              <div class="who">Daddy</div>
+            </div>
+          </f7-col>
+        </f7-row>
+      </f7-block>
+    </div>
   </f7-page>
 </template>
 
@@ -69,4 +74,18 @@ function onChangeSleepCompanyClicked( newWho ) {
 </script>
 
 <style lang="scss" scoped>
+.who {
+  display: flex;
+  text-align: center;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  box-sizing: border-box;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+
+  line-height: calc(var(--f7-button-height) - var(--f7-button-border-width, 0) * 2);
+  font-size: 11px;
+}
 </style>
